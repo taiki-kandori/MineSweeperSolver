6587f0135630eeb52059a7cba44ed5c03f47ee6f
shinshu68 <shinshu68@gmail.com>
2020-02-20 16:27:32 +0900

    :sparkles: getitemとstrの挙動を追加

--- minesweeper_solver/field.py
+++ minesweeper_solver/field.py
@@ -1,5 +1,14 @@
+from minesweeper_solver.square import Square
+
 class Field(object):
     """NxMマスのフィールドを表現する"""
     def __init__(self, n, m):
         self.field = [[Square(j, i) for i in range(n)] for j in range(m)]
+
+    def __getitem__(self, i):
+        return self.field[i]
+
+    def __str__(self):
+        line = ["".join(map(str, i)) for i in self.field]
+        return '\n'.join(map(str, line))
 
